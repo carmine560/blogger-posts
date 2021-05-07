1be3f3fb9127ce94838f82670bdef1cde84855a6
carmine <40491810+carmine560@users.noreply.github.com>
2021-05-08 04:04:40 +0900

    Move encryption functions to encrypted_configuration.sh

--- configuration.sh
+++ configuration.sh
@@ -1,5 +1,5 @@
 ## @file
-## @brief Read and write the plain or encrypted configuration file.
+## @brief Read and write the plain text configuration file.
 
 if [ -d "$HOME/.config" ]; then
     readonly CONFIGURATION=$HOME/.config/${0##*/}
@@ -26,55 +26,5 @@ EOF
         else
             exit
         fi
-    fi
-}
-
-## @fn cfg_initialize_encryption()
-## @brief Execute the existing encrypted configuration file, or create
-## it.
-cfg_initialize_encryption() {
-    readonly GPG_OPTIONS='--default-recipient-self --batch --yes'
-    if [ -f "$CONFIGURATION.gpg" ]; then
-        eval "$(gpg -dq "$CONFIGURATION.gpg" || echo exit $?)"
-    else
-        read -p "$CONFIGURATION.gpg does not exist.  Create it? [Y/n] "
-        if [ -z "$REPLY" ] || [[ $REPLY =~ ^[Yy] ]]; then
-            cat <<EOF | gpg -e $GPG_OPTIONS -o "$CONFIGURATION.gpg" || exit
-$default_configuration
-EOF
-            exit
-        else
-            exit
-        fi
-    fi
-}
-
-## @fn cfg_set_encrypted_value()
-## @brief Store the value of a variable in the encrypted
-## configuration.
-## @details Multiple pairs of parameters are allowed.
-## @param $regex A regular expression for a variable.
-## @param $replacement A replacement for the value.
-cfg_set_encrypted_value() {
-    if [ $# != 0 -a $(($# % 2)) == 0 ]; then
-        cp "$CONFIGURATION.gpg" "$CONFIGURATION.gpg.bak" || exit
-        local index=0
-        local parameters=("$@")
-        local sed_commands
-        while [ "$index" -lt ${#parameters[*]} ]; do
-            if [ -z "$sed_commands" ]; then
-                sed_commands="s/^(\s*${parameters[index]}=).*$/\1${parameters[++index]}/"
-            else
-                sed_commands="$sed_commands; s/^(\s*${parameters[index]}=).*$/\1${parameters[++index]}/"
-            fi
-            ((++index))
-        done
-        gpg -dq "$CONFIGURATION.gpg.bak" |
-            sed -E "$sed_commands" |
-            gpg -e $GPG_OPTIONS -o "$CONFIGURATION.gpg" || exit
-        rm "$CONFIGURATION.gpg.bak" || exit
-    else
-        echo Usage: ${FUNCNAME[0]} VARIABLE VALUE [VARIABLE VALUE ...] >&2
-        exit 2
     fi
 }
